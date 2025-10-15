d8bc1a1c77e1357f8e8d350bd364563519de8310
Tom Hardy <tom.hardy1219@gmail.com>
2025-10-15 23:43:33 +0100

    fix: wrapper now takes argparse

--- notebooks/week2/ifutools_wrapper.py
+++ notebooks/week2/ifutools_wrapper.py
@@ -7,29 +7,36 @@ import sys
 import plotfancy as pf
 from matplotlib.patches import Circle
 from astropy.visualization import ZScaleInterval
+import argparse
 
 pf.housestyle_rcparams()
 
 from IFU_tools import analyse_galaxy_spectrum
 
-CUBE_PATH = '../../cubes/macs0159m34_COMBINED_CUBE_MED_FINAL.fits'
-# CUBE_PATH = '../../cubes/s780_COMBINED_CUBE_MED_FINAL.fits'
-TITLE = 'MACS'
-RADIUS_ARCSEC = .6
-Z = 0.69
+def main():
+    parser = argparse.ArgumentParser(description="Analyse galaxy spectrum from a MUSE cube.")
+    parser.add_argument('--cube_path', type=str, default='../../cubes/macs0159m34_COMBINED_CUBE_MED_FINAL.fits', help='Path to the MUSE cube')
+    parser.add_argument('--title', type=str, default='MACS', help='Title for the analysis')
+    parser.add_argument('--radius', type=float, default=0.6, help='Radius in arcseconds')
+    parser.add_argument('--Z', type=float, default=0.69, help='Redshift guess')
+    parser.add_argument('--ra_hms', type=str, default='01h59m06.05s', help='Right Ascension in hms format')
+    parser.add_argument('--dec_dms', type=str, default='-34d13m03.8s', help='Declination in dms format')
 
-ra_hms = '01h59m06.05s'
-dec_dms = '-34d13m03.8s'
-coords = SkyCoord(ra_hms, dec_dms, frame='icrs')
-RA_DEG = coords.ra.deg
-DEC_DEG = coords.dec.deg
+    args = parser.parse_args()
 
-spec, lf1 = analyse_galaxy_spectrum(
-    cube_path=CUBE_PATH,
-    ra=RA_DEG,
-    dec=DEC_DEG,
-    radius=RADIUS_ARCSEC,
-    title=TITLE,
-    z_guess=Z,
-    pref=ra_hms.replace('.','p')+'_'+dec_dms.replace('.','p')
-)
+    coords = SkyCoord(args.ra_hms, args.dec_dms, frame='icrs')
+    RA_DEG = coords.ra.deg
+    DEC_DEG = coords.dec.deg
+
+    spec, lf1 = analyse_galaxy_spectrum(
+        cube_path=args.cube_path,
+        ra=RA_DEG,
+        dec=DEC_DEG,
+        radius=args.radius,
+        title=args.title,
+        z_guess=args.Z,
+        pref=args.ra_hms.replace('.','p')+'_'+args.dec_dms.replace('.','p')
+    )
+
+if __name__ == '__main__':
+    main()
